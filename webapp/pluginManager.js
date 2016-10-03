299558f246cebecb5a03cd66cbefd62aa0dfc1f8
Stefano Pirra <Stefano.Pirra@ddway.it>
2016-10-03 10:53:21 +0200

    better plugin manage, with error handler

--- webapp/pluginManager.js
+++ webapp/pluginManager.js
@@ -1,18 +1,28 @@
 var fs = require('fs');
 
+function tryToLoad(plugin, app) {
+  // init plugin routes
+  var pluginPath = './plugins/' + plugin + '/router.js';
+  fs.stat(pluginPath, (err, stats) => {
+    if (err) {
+      console.log('PluginManager ERROR','Router *' + plugin + '* NOT found (skipped ...)');
+      console.log('PluginManager ERROR', err.message);
+    }
+    else {
+      var pluginRouter = require(pluginPath);
+      if (pluginRouter && pluginRouter.init) pluginRouter.init(app);
+      console.log('PluginManager', '*' + plugin + '* found and loaded.');
+    }
+  });
+}
+
 exports.load = function(app) {
   return new Promise(function (resolve, reject) {
     fs.readdir('plugins', (err, files) => {
       if (err) reject(err);
       else {
         for (i in files) {
-          var plugin = files[i];
-          
-          // init plugin routes
-          var pluginPath = './plugins/' + plugin + '/router';
-          var pluginRouter = require(pluginPath);
-          if (pluginRouter && pluginRouter.init) pluginRouter.init(app);
-          console.log('plugins ' + plugin + ' loaded ...');
+          tryToLoad(files[i], app)
         }
         resolve(files);
       }
